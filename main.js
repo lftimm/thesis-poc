63a61619b8c34e195e58f14a7da831a417651d69
lftimm <lftimm1902@gmail.com>
2025-10-06 21:23:30 -0300

    sphere with simple shaders

--- main.js
+++ main.js
@@ -1,5 +1,4 @@
 import * as THREE from 'three';
-import { OrbitControls } from 'three/examples/jsm/Addons.js';
 
 // CONSTANTS
 const width = window.innerWidth;
@@ -11,49 +10,27 @@ const farPlane = 1000;
 
 // SCENE DEFINITIONS
 const scene = new THREE.Scene();
+
 const camera = new THREE.PerspectiveCamera(cameraFov,ratio,nearPlane,farPlane);
-camera.position.x = 10;
-camera.position.y = 0;
+camera.position.z = 5;
 
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(width, height);
 renderer.setAnimationLoop(animate);
 document.body.appendChild(renderer.domElement);
 
-renderer.physicallyCorrectLights = true;
-renderer.toneMapping = THREE.ACESFilmicToneMapping;
-renderer.toneMappingExposure = 1;
-
-const control = new OrbitControls(camera,renderer.domElement);
-control.enableDamping = true;
-control.dampingFactor = 0.05;
-
-const gridHelper = new THREE.GridHelper(50,50);
-const axesHelper = new THREE.AxesHelper();
-scene.add(gridHelper);
-scene.add(axesHelper);
-
-const color = 0xFFFFFF;
-const intensity = 1;
-
-const ambientlight = new THREE.AmbientLight(color, intensity/2);
-const light = new THREE.DirectionalLight(color, intensity*2);
-light.position.set(-1, 2, 4);
-
-scene.add(ambientlight);
-scene.add(light);
-
 // SCENE OBJECTS
-const texture = new THREE.TextureLoader().load( "resources/concrete.jpg" );
+const vertexShader = document.getElementById("vertexShader").textContent;
+const fragmentShader = document.getElementById("fragmentShader").textContent;
 
-const geometry = new THREE.CylinderGeometry(1,1,10,50);
+const geometry = new THREE.SphereGeometry(1);
+const material = new THREE.ShaderMaterial({
+    vertexShader,
+    fragmentShader
+});
 
-const material = new THREE.MeshStandardMaterial({map:texture});
-
-const object = new THREE.Mesh(geometry,material);
-object.rotation.x = 3.14/2;
-object.position.y += 5;
-scene.add(object);
+const cube = new THREE.Mesh(geometry,material);
+scene.add(cube);
 
 // MAIN LOOP
 function animate() {
